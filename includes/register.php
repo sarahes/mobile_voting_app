ca12d67b90ea3f943063d54803e445443a7280ef
Adam Boerema <adamboerema@gmail.com>
2012-02-23 13:23:33 -0500

    Rearranged files into index.php

---
+++ includes/register.php
@@ -0,0 +1,16 @@
+<p>Register to vote using your name and unique voter id: </p>
+				
+	<form action="scripts/login.php" method="post">
+		<ul>
+			<li>
+				<label for="voterId">Voter Registration Number: </label>
+				<input type="text" name="voterId" id="voterId" value="" />					
+			</li>
+			<li>
+				<label for="name">First Name: </label>
+				<input type="text" name="name" id="name" value="" />
+			</li>
+			<li><input type="submit" /></li>
+		</ul>		
+	</form>
+
--- index.php
+++ index.php
@@ -1,19 +1,38 @@
-<?php include_once('includes/header.php'); ?>
-			
-	<p>Register to vote using your name and unique voter id: </p>
-				
-	<form action="scripts/login.php" method="post">
-		<ul>
-			<li>
-				<label for="voterId">Voter Registration Number: </label>
-				<input type="text" name="voterId" id="voterId" value="" />					
-			</li>
-			<li>
-				<label for="name">First Name: </label>
-				<input type="text" name="name" id="name" value="" />
-			</li>
-			<li><input type="submit" /></li>
-		</ul>		
-	</form>
-			
-<?php include_once('includes/footer.php'); ?>		
+<!DOCTYPE html>
+<html>
+<head>
+<title>DIG 4104c Project 2 - Adam Boerema - Sarah Sheehan - Melinda Velasquez</title>
+	<meta charset="utf-8" />
+  	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="css/themes/custom-theme.css" />
+	<link rel="stylesheet" href="http://code.jquery.com/mobile/1.0.1/jquery.mobile-1.0.1.min.css" />
+	<script src="http://code.jquery.com/jquery-1.6.4.min.js"></script>
+	<script src="http://code.jquery.com/mobile/1.0.1/jquery.mobile-1.0.1.min.js"></script>
+</head>
+<body>
+<div data-role="page" id="userID">
+  <div data-role="header">
+    <h1>"Voting ID"</h1>
+  </div>
+  <div data-role="content" class="center">
+  	<?php include_once 'include/register.php' ?>
+  <div>
+</div>
+<div data-role="page" id="vote">
+  <div data-role="header">
+    <h1>"Choose a Candidate"</h1>
+  </div>
+  <div data-role="content" class="center">
+  	<?php include_once 'include/vote.php' ?>
+  <div>
+</div>
+<div data-role="page" id="vote">
+  <div data-role="header">
+    <h1>"Vote Totals"</h1>
+  </div>
+  <div data-role="content" class="center">
+		<?php include_once 'include/results.php' ?>
+  <div>
+</div>
+</body>
+</html>

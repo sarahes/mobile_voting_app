ad82cf22bd664214f01771e77422c2cb1a566b78
Adam Boerema <adamboerema@gmail.com>
2012-02-29 00:39:36 -0500

    Removed some database info in admin

--- includes/admin.php
+++ includes/admin.php
@@ -1,16 +1,6 @@
 <?php 
 	function getVoters(){
 		include('config/db.php');
-		
-	 	$v = $conn->query("select * from voters");          
-        $v->setFetchMode(PDO::FETCH_OBJ);
-        
-        $voters = array();
-		while ($voter = $v->fetch())
-        {				
-        	print $voter;					
-        }
-        
                         
 }
 ?>
